897795ba25ab111f5fe40751f0f05a2ead31a685
João Adriano Freitas <jaafreitas@gmail.com>
2017-10-29 22:54:42 -0200

    Desnecessário o uso do __init__ neste caso.

--- villares/BandeirinhaCirculo.py
+++ villares/BandeirinhaCirculo.py
@@ -1,9 +1,6 @@
 from Bandeirinha import Bandeirinha
 
 class BandeirinhaCirculo(Bandeirinha):
-    def __init__(self, px, py, ptamanho=None):
-        Bandeirinha.__init__(self, px, py, ptamanho)
-        
     def desenha(self):
         Bandeirinha.desenha(self)
         fill(0)
